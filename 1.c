a109211ea11ca612100e9590f99c7cf4f162de62
hossen250 <66531322+hossen250@users.noreply.github.com>
2020-11-03 10:57:02 -0800

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,22 +1,31 @@
 /*
-#Problem Set-1
-1.Scan N (take input N from user)values from user into an array.
+Problem Set -02
+1.Insert any value at tenth[10th] index of array.
 1915002508, Md Sazzad Hossen
 Data Structure Lab.
 */
-
 #include<stdio.h>
 int main()
 {
-    int a[100];
-    int N,i;
-    scanf("%d",&N);
-    if(N>0 && N<100)
-    {
-      for(i=0; i<N; i++)
-      {
-          scanf("%d",&a[i]);
-      }
-   return 0;
-    }
+    int a[100] = {7,9,3,6,5,6,7,8,9,10};
+    int i,max_size = 100, s=10;
 
+    printf("Before Operation:\n");
+    for(i=0;i<s;i++)
+    {
+       printf("%d ",a[i]);
+    }
+    printf("\n");
+
+    a[10] = 50;
+    s++;
+
+    printf("After Operation:\n");
+    for(i=0;i<s;i++)
+    {
+       printf("%d ",a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
